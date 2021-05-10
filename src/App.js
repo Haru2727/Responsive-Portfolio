427550a0d8a45135b90202ff4933436781b5dab5
Arash Saeedi <saeediarash84@gmail.com>
2021-05-10 16:56:27 -0400

    started from sctrach following a tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-// import Home from "./pages/Home/Home";
-// import Portfolio from "./pages/Portfolio/Portfolio";
-// import Contact from "./pages/Contact/Contact";
-// import './App.css';
+import './App.scss';
+import  NavBar  from './components/NavBar'
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Header />
-          {/* <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/portfolio" component={Portfolio} />
-          <Route exact path="/contact" component={Contact} /> */}
-        <Footer />
+    <div className="App">
+      <div className="sidebar">
+        <NavBar />
       </div>
-    </Router>
+      <div className="main-content"></div>
+    </div>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import avatar from '../../src/images/profile.jpg';
+import { NavLink } from 'react-router-dom';
+
+function NavBar() {
+    return (
+        <div className="NavBar">
+            <nav className="nav">
+                <div className="profile">
+                    <img src={avatar} alt="Arash Saeedi" />
+                </div>
+                <ul className="nav-items">
+                    <li className="nav-item">
+                        <NavLink to="/" exact activeClassName="active">
+                            Home
+                         </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/about" exact activeClassName="active">
+                            About
+                         </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/portfolio" exact activeClassName="active">
+                            Portfolio
+                         </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/contact" exact activeClassName="active">
+                            Contact
+                         </NavLink>
+                    </li>
+                </ul>
+                <footer className="footer">
+                <p>
+                    @2021 Arash Saeedi
+                    (770)294-5935
+                    saeediarash84@gmail.com
+                </p>
+            </footer>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;
